d6a44f2227a23da9a8c1f4edab68f6eaa1af07e9
Chaedooseok <68419018+Chaedooseok@users.noreply.github.com>
2020-12-26 20:34:26 +0900

    Add files via upload

--- src/User.js
+++ src/User.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import useAsync from './useAsync';
+//import useAsync from './useAsync';
+import { useAsync } from "react-async";
 
-async function getUser(id){
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-    return response.data;   
+async function getUser({ id }) {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  return response.data;
 }
-function User({id}){
-    const [state] = useAsync(()=> getUser(id), [id]);
+function User({ id }) {
+  const { data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
+  });
 
-    const{loading, data:user, error} = state;
-    if(loading) return <div>로딩중...</div>
-    if(error) return <div>에러가 발생...</div>
-    if(!user) return null
+  if (isLoading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생...</div>;
+  if (!user) return null;
 
-    return(
-        <div>
-            <h2>{user.username}</h2>
-            <p>
-                <b>Email : </b> {user.email}
-            </p>
-
-        </div>
-    )
+  return (
+    <div>
+      <h2>{user.username}</h2>
+      <p>
+        <b>Email : </b> {user.email}
+      </p>
+    </div>
+  );
 }
-
 
 export default User;
--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 import User from "./User";
 
 async function getUsers() {
@@ -10,14 +10,16 @@ async function getUsers() {
   return response.data;
 }
 function Users() {
-  const [state, refetch] = useAsync(getUsers, [], true);
   const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, reload, run } = useAsync({
+    //promiseFn: getUsers,
+    deferFn: getUsers, //알에거와 run 은 처음에 버튼 눌렀을때 호출 하는거...
+  });
 
-  const { loading, data: users, error } = state;
-
-  if (loading) return <div>로딩중</div>;
+  if (isLoading) return <div>로딩중</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  //if (!users) return <button onClick={reload}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
   return (
     <>
       <ul>
@@ -27,7 +29,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={run}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
