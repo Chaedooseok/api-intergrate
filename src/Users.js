9cfef104ef4de3b11155c07a98299426b286c404
Chaedooseok <68419018+Chaedooseok@users.noreply.github.com>
2020-12-27 23:43:18 +0900

    Add files via upload

--- src/Users.js
+++ src/Users.js
@@ -1,25 +1,22 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { useAsync } from "react-async";
-import User from "./User";
 
-async function getUsers() {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/users/"
-  );
-  return response.data;
-}
+import User from "./User";
+import { getUsers, useUsersDispatch, useUsersState } from "./UsersContext";
+
 function Users() {
   const [userId, setUserId] = useState(null);
-  const { data: users, error, isLoading, reload, run } = useAsync({
-    //promiseFn: getUsers,
-    deferFn: getUsers, //알에거와 run 은 처음에 버튼 눌렀을때 호출 하는거...
-  });
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
 
-  if (isLoading) return <div>로딩중</div>;
+  const { loading, data: users, error } = state.users;
+
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
+  if (loading) return <div>로딩중</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   //if (!users) return <button onClick={reload}>불러오기</button>;
-  if (!users) return <button onClick={run}>불러오기</button>;
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
   return (
     <>
       <ul>
@@ -29,7 +26,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={run}>다시 불러오기</button>
+      <button onClick={fetchData}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
